759e7fbb578d158e924adfa9f836ea87405e278f
Anzokk <antoninoplaia5.ap@gmail.com>
2023-01-19 03:05:54 +0100

    Updat
    deleting

--- Progetto Finale Primo/Progetto solo/Progetto solo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.progetto {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.progetto to javafx.fxml;
-    exports com.example.progetto;
-}
--- Progetto Finale Primo/prove pol/Official view/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.officialview {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.xml;
-
-
-    opens com.example.officialview to javafx.fxml;
-    exports com.example.officialview;
-}
